d203f0d484fa85f553b61120ec4cb72fcb8e4e8a
Chinmay Dhekne <cdhekne@users.noreply.github.com>
2015-08-18 14:30:14 -0700

    Added ASU ID in Scra[[er

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import time
 
 headers = ["Name","Department","Total Ratings","Overall Quality","Easiness","Hot"]
 #Dictionary of school ids (keys) that map to tuple of school name and number of pages
-colleges = {"580":("MIT",16),"1222":("Yale",23),"181":("CMU",28), "1085":("UChicago",28),"1040":("Tufts",46), "1350":("Duke",84),"1255":("UTexas",84),"953":("Stanford",32),"799":("Rice",17),"780":("Princeton",16)}
+colleges = {"580":("MIT",16),"1222":("Yale",23),"181":("CMU",28), "1085":("UChicago",28),"1040":("Tufts",46), "1350":("Duke",84),"1255":("UTexas",84),"953":("Stanford",32),"799":("Rice",17),"780":("Princeton",16),"45":("ASU",200)}
 
 for sid in colleges.keys():
     college,pages = colleges[sid]
