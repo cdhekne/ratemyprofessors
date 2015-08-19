49d026d7c5ea40b400a01c9322459f50accf4905
Chinmay Dhekne <cdhekne@users.noreply.github.com>
2015-08-19 11:22:08 -0700

    more changes

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import time
 
 headers = ["Name","Department","Total Ratings","Overall Quality","Easiness","Hot"]
 #Dictionary of school ids (keys) that map to tuple of school name and number of pages
-colleges = {"45":("ASU",20000)}
+colleges = {"45":("ASU",168)}
 
 for sid in colleges.keys():
     college,pages = colleges[sid]
